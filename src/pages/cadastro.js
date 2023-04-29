31ce52a5f44f41ed20514d728b9853ea3f2bb69f
Felipe-Tino-Lima <90803786+Felipe-Tino-Lima@users.noreply.github.com>
2023-04-29 12:48:44 -0300

    Add files via upload

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -5,9 +5,6 @@ export default function Cadastro() {
 
     const [nome, setNome] = useState('');
     const [telefone, setTelefone] = useState('');
-    const [email, setEmail] = useState('');
-    const [nomecartao, setNomeCartao] = useState('');
-    const [numerocartao, setNumeroCartao] = useState('');
 
     function handleNomeChange(event) {
         setNome(event.target.value);
@@ -17,18 +14,6 @@ export default function Cadastro() {
         setTelefone(event.target.value);
     }
 
-    function handleEmailChange(event) {
-        setEmail(event.target.value);
-    }
-
-    function handleNomeCartaoChange(event) {
-        setEmail(event.target.value);
-    }
-
-    function handleNumeroCartaoChange(event) {
-        setEmail(event.target.value);
-    }
-
     function handleSubmit(event) {
         event.preventDefault();
         // enviar dados de cadastro para o servidor aqui
@@ -36,21 +21,22 @@ export default function Cadastro() {
     }
 
     return (
-                <form onSubmit={handleSubmit}>
-                <div class="form-floating mb-3">
-                    <label for="floatingInput">Nome completo
-                    
-                    <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" value={nome} onChange={handleNomeChange}></input>
-                    </label>
-                        </div>
-                <div class="form-floating">
-                    <label for="floatingPassword">Password
-                    
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
-                    </label>
-                        </div>
-                    <br />
-                    <button type="submit">Enviar</button>
-                </form>
+ 
+        <div className="form-custom">
+           <Title title={"Cadastro de Assinante"} />
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Nome:
+                    <input type="text" value={nome} onChange={handleNomeChange} />
+                </label>
+                <br />
+                <label>
+                    Telefone:
+                    <input type="text" value={telefone} onChange={handleTelefoneChange} />
+                </label>
+                <br />
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
     );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Title from './../components/Title/index';
+import Card from './../components/Card/index';
 
 function Home() {
-  return (
-    <div>
-       <Title
+    return (
+        <div>
+            <Title
                 title={"Catálogo de Filmes"}
-                 />
+                text={"Filmes Disponíveis"} />
             {/* <Title title=""/> */}
-    </div>
-  );
+            <Card />
+        </div>
+    )
 }
-
 export default Home;
