964521d5f090087c015df1669f0eecac1620c53f
Esdras Lemuel <esdras410maneiro@gmai.lc.com>
2023-04-29 02:42:37 -0300

    cadastro incompleto

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -5,6 +5,9 @@ export default function Cadastro() {
 
     const [nome, setNome] = useState('');
     const [telefone, setTelefone] = useState('');
+    const [email, setEmail] = useState('');
+    const [nomecartao, setNomeCartao] = useState('');
+    const [numerocartao, setNumeroCartao] = useState('');
 
     function handleNomeChange(event) {
         setNome(event.target.value);
@@ -14,6 +17,18 @@ export default function Cadastro() {
         setTelefone(event.target.value);
     }
 
+    function handleEmailChange(event) {
+        setEmail(event.target.value);
+    }
+
+    function handleNomeCartaoChange(event) {
+        setEmail(event.target.value);
+    }
+
+    function handleNumeroCartaoChange(event) {
+        setEmail(event.target.value);
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
         // enviar dados de cadastro para o servidor aqui
@@ -21,22 +36,21 @@ export default function Cadastro() {
     }
 
     return (
- 
-        <div className="form-custom">
-           <Title title={"Cadastro de Assinante"} />
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Nome:
-                    <input type="text" value={nome} onChange={handleNomeChange} />
-                </label>
-                <br />
-                <label>
-                    Telefone:
-                    <input type="text" value={telefone} onChange={handleTelefoneChange} />
-                </label>
-                <br />
-                <button type="submit">Enviar</button>
-            </form>
-        </div>
+                <form onSubmit={handleSubmit}>
+                <div class="form-floating mb-3">
+                    <label for="floatingInput">Nome completo
+                    
+                    <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" value={nome} onChange={handleNomeChange}></input>
+                    </label>
+                        </div>
+                <div class="form-floating">
+                    <label for="floatingPassword">Password
+                    
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
+                    </label>
+                        </div>
+                    <br />
+                    <button type="submit">Enviar</button>
+                </form>
     );
 }
